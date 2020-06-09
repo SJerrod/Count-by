a907c8fbf1b699b8434f58d892195f9edfd1981c
Nikolas Kososik <nikolas.kososik@gmail.com>
2020-06-09 11:57:28 -0700

    convert inputs to numbers
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
   $("#form1").submit(function(event) {
     event.preventDefault();
     
-    let countTo = $("input#countTo").val();
-    let countBy = $("input#countBy").val();
-    console.log(typeof countTo);
+    let input1 = $("input#countTo").val();
+    let input2 = $("input#countBy").val();
+    let countTo = parseInt(input1);
+    let countBy = parseInt(input2);
     let arrayCountTo = []
     
     function divide(countTo, countBy) {
@@ -12,8 +13,8 @@ $(document).ready(function() {
     }
     
     // for ( let i=0; i<countTo.length; i +=1) {
-      
-      // }
+    //   dividedArray.push(countTo[index] / countBy);
+    //   }
       
       let result = divide(countTo, countBy);
       $("#output").show(result);
