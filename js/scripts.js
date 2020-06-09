1f96db52eef08e1003f29ccaf0d4f72926105bb8
Nikolas Kososik <nikolas.kososik@gmail.com>
2020-06-09 13:32:06 -0700

    add for loop to scripts to count through user inputs
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,19 @@
 $(document).ready(function() {
   $("#form1").submit(function(event) {
     event.preventDefault();
-    
+
     let input1 = $("input#countTo").val();
     let input2 = $("input#countBy").val();
     let countTo = parseInt(input1);
     let countBy = parseInt(input2);
-    let arrayCountTo = []
     
-    function divide(countTo, countBy) {
-      return countTo / countBy;
+    let outputArray = []
+    
+    for (let i = 0; i <= countTo; i += countBy) {
+      outputArray.push(" " + [i] + ",");
+      console.log(outputArray);
     }
-    
-    // for ( let i=0; i<countTo.length; i +=1) {
-    //   dividedArray.push(countTo[index] / countBy);
-    //   }
-      
-      let result = divide(countTo, countBy);
-      $("#output").show(result);
+
+    $("#output").after(outputArray);
   });
 });
